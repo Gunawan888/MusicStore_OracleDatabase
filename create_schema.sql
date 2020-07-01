f5b2573d5fe1c2231692422fab23319db988016b
David Gunawan <43849473+Gunawan888@users.noreply.github.com>
2020-07-01 11:45:58 +1000

    Add functions

--- create_schema.sql
+++ create_schema.sql
@@ -62,6 +62,44 @@ create table albums of album_type
 object id system generated
 nested table albumReviews store as store_reviews 
 / 
-
+-- Implement function 
+create or replace type body album_type 
+as 
+    member function discountPrice return number is
+    v_discount number(9,2);
+        BEGIN
+             null; 
+        END;
+    member function containsText (pString1 varchar2, pString2 varchar2) return integer is
+        BEGIN
+            if INSTR(lower(pString1),lower(pString2)) > 0  then return 1;
+            else return 0; 
+            end if;
+        END;
+END;
+/
+-- Overriding Function
+create or replace type body disk_type as 
+overriding member function discountPrice
+    return number is v_discount number(9,2);
+    begin
+        if mediaType = 'Vinyl' and (albumReleaseDate < (Sysdate - interval '1' year)) then return (0.8*albumPrice);       
+        else if mediaType = 'Audio CD' and (albumReleaseDate < (Sysdate - interval '1' year)) then return (0.85*albumPrice); 
+        else return albumPrice;
+        end if;
+        end if;
+    end;
+end;
+/
+create or replace type body mp3_type as 
+overriding member function discountPrice
+    return number is v_discount number(9,2);
+    begin 
+        if albumReleaseDate < (Sysdate - interval '2' year) then return (0.9*albumPrice);
+        else return albumPrice;
+        end if;
+    end;
+end;
+/
 
 
